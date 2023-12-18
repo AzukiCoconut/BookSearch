e206f7ba6cfcdda941c17e451fb02e997497c2b7
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-12-17 21:47:19 -0400

    Added and tested the GraphQL schemas

--- server/models/User.js
+++ server/models/User.js
@@ -43,8 +43,8 @@ userSchema.pre('save', async function (next) {
 });
 
 // custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+userSchema.methods.isCorrectPassword = function (password) {
+    return bcrypt.compare(password, this.password);
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
