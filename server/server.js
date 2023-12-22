e290e60910cc38cc5a06a41fac644fb5408faa0c
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-12-22 12:29:54 -0400

    Pushing to Heroku

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
